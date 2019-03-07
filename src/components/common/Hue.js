0e362f0663fea4004e9289785aa0cfd28bdf1bd0
Ed Lee <jonexiu@gmail.com>
2019-03-07 17:21:37 +0800

    slider touch

--- src/components/common/Hue.js
+++ src/components/common/Hue.js
@@ -8,19 +8,26 @@ export class Hue extends (PureComponent || Component) {
   }
 
   handleChange = (e) => {
-    const change = hue.calculateChange(e, this.props.direction, this.props.hsl, this.container)
-    change && typeof this.props.onChange === 'function' && this.props.onChange(change, e)
+    const change = hue.calculateChange(
+      e,
+      this.props.direction,
+      this.props.hsl,
+      this.container
+    )
+    change &&
+      typeof this.props.onChange === 'function' &&
+      this.props.onChange(change, e)
   }
 
-  handleMouseDown = (e) => {
-    this.handleChange(e)
-    window.addEventListener('mousemove', this.handleChange)
-    window.addEventListener('mouseup', this.handleMouseUp)
-  }
+  // handleMouseDown = (e) => {
+  //   this.handleChange(e)
+  //   window.addEventListener('mousemove', this.handleChange)
+  //   window.addEventListener('mouseup', this.handleMouseUp)
+  // }
 
-  handleMouseUp = () => {
-    this.unbindEventListeners()
-  }
+  // handleMouseUp = () => {
+  //   this.unbindEventListeners()
+  // }
 
   unbindEventListeners() {
     window.removeEventListener('mousemove', this.handleChange)
@@ -30,52 +37,57 @@ export class Hue extends (PureComponent || Component) {
   render() {
     const { direction = 'horizontal' } = this.props
 
-    const styles = reactCSS({
-      'default': {
-        hue: {
-          absolute: '0px 0px 0px 0px',
-          borderRadius: this.props.radius,
-          boxShadow: this.props.shadow,
+    const styles = reactCSS(
+      {
+        default: {
+          hue: {
+            absolute: '0px 0px 0px 0px',
+            borderRadius: this.props.radius,
+            boxShadow: this.props.shadow,
+          },
+          container: {
+            padding: '0 2px',
+            position: 'relative',
+            height: '100%',
+            borderRadius: this.props.radius,
+            touchAction: 'pan-x',
+          },
+          pointer: {
+            position: 'absolute',
+            left: `${ (this.props.hsl.h * 100) / 360 }%`,
+          },
+          slider: {
+            marginTop: '1px',
+            width: '4px',
+            borderRadius: '1px',
+            height: '8px',
+            boxShadow: '0 0 2px rgba(0, 0, 0, .6)',
+            background: '#fff',
+            transform: 'translateX(-2px)',
+          },
         },
-        container: {
-          padding: '0 2px',
-          position: 'relative',
-          height: '100%',
-          borderRadius: this.props.radius,
-        },
-        pointer: {
-          position: 'absolute',
-          left: `${ (this.props.hsl.h * 100) / 360 }%`,
-        },
-        slider: {
-          marginTop: '1px',
-          width: '4px',
-          borderRadius: '1px',
-          height: '8px',
-          boxShadow: '0 0 2px rgba(0, 0, 0, .6)',
-          background: '#fff',
-          transform: 'translateX(-2px)',
+        vertical: {
+          pointer: {
+            left: '0px',
+            top: `${ -((this.props.hsl.h * 100) / 360) + 100 }%`,
+          },
         },
       },
-      'vertical': {
-        pointer: {
-          left: '0px',
-          top: `${ -((this.props.hsl.h * 100) / 360) + 100 }%`,
-        },
-      },
-    }, { vertical: direction === 'vertical' })
+      { vertical: direction === 'vertical' }
+    )
 
     return (
       <div style={ styles.hue }>
         <div
           className={ `hue-${ direction }` }
           style={ styles.container }
-          ref={ container => this.container = container }
-          onMouseDown={ this.handleMouseDown }
+          ref={ (container) => (this.container = container) }
+          // onMouseDown={ this.handleMouseDown }
           onTouchMove={ this.handleChange }
           onTouchStart={ this.handleChange }
         >
-          <style>{ `
+          <style>
+            {`
             .hue-horizontal {
               background: linear-gradient(to right, #f00 0%, #ff0 17%, #0f0
                 33%, #0ff 50%, #00f 67%, #f0f 83%, #f00 100%);
@@ -89,13 +101,14 @@ export class Hue extends (PureComponent || Component) {
               background: -webkit-linear-gradient(to top, #f00 0%, #ff0 17%,
                 #0f0 33%, #0ff 50%, #00f 67%, #f0f 83%, #f00 100%);
             }
-          ` }</style>
+          `}
+          </style>
           <div style={ styles.pointer }>
-            { this.props.pointer ? (
+            {this.props.pointer ? (
               <this.props.pointer { ...this.props } />
             ) : (
               <div style={ styles.slider } />
-            ) }
+            )}
           </div>
         </div>
       </div>
